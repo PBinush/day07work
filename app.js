de12529da7ec3683cffd857177314722ba7f80d8
PBinush <pasidbinush@gmail.com>
2024-09-12 17:35:54 +0530

    second commit

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+// function addition(){
+//      = number1 + number2;
+// }
+// function subtraction() {
+//      = number1-number2;
+// }
+// function multiplication() {
+//      = number1*number2;
+// }
+// function division() {
+//      = number1/number2;
+// }
+
 console.log("hi");
-function calc(){
+function calc() {
     let number1 = new Number(document.getElementById("number1").value);
     let number2 = new Number(document.getElementById("number2").value);
-   
-    let op = document.getElementById("op").value;
+
     let lblOutput = document.getElementById("output");
-     
-    console.log(typeof number1);
-    console.log(typeof number2);
-    
-      
-    lblOutput.innerHTML = number1+number2 ;
-}
+    let op = document.getElementById("op").value;
+
+    switch (op) {
+        case "+": lblOutput.innerHTML = number1 + number2; break;
+        case "-": lblOutput.innerHTML = number1 - number2; break;
+        case "*": lblOutput.innerHTML = number1 * number2; break;
+        case "/": lblOutput.innerHTML = number1 / number2; break;
+    }
+} 
